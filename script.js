3ec0c2b080d6a901f7307107a5ecaf404c998d39
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-09-14 22:42:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+const accessKey ="JLxAxV-k70kXxSdilw861qTDN4XaiCr7hQlqikzCYRA";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchresults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page=1;
+
+async function searchImages(){
+    inputData =  inputEl.value;
+    const url =  'https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}';
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results =data.results;
+
+    if(page === 1){
+        searchresults.innerHTML ="";
+    }
+    results.map((result)=>{
+        const imageWrapper =document.createElement("div");
+        imageWrapper.classList.add("result2");
+        const image =document.createElement("img");
+        image.src = result.urls.small;
+        image.alt= result.alt.description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.link.html;
+        imageLink.target = "_blank";
+        imageLink.text=result.alt_description;
+        
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchresults.appendChild(imageWrapper);
+                });
+     page++;
+     if (page > 1) {
+        showMore.style.display = "block";
+
+     }
+    }
+
+     formEl.addEventListener("submit",(event) =>{
+        event.preventDefault()
+        page=1;
+        searchImages();
+     });
+     showMore.addEventListener("click",() =>{
+        
+        searchImages();
+     });
